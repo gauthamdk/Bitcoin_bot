578541d38bdd9ea185ff9ef124e4fd3c58e3da0e
Gautham <gauthamkumar.gk.gk@gmail.com>
2020-04-25 01:46:39 +0400

    Added opeing webpage and clicking play now

--- bitcoin_bot/bitcoin_bot.py
+++ bitcoin_bot/bitcoin_bot.py
@@ -1,5 +1,22 @@
 import selenium
+from selenium import webdriver
+from details import pw
+
+from time import sleep
 
 class BitcoinBot:
 
-    def init
+    def __init__(self, username, pw):
+
+        chromedriver = '/Users/gautham/Google Drive (gdk244@nyu.edu)/Coding/Bitcoin_bot/bitcoin_bot/chromedriver'
+        self.driver = selenium.webdriver.Chrome(executable_path = chromedriver)
+        self.driver.get("https://www.bustabit.com")
+        sleep(2)
+
+        self.driver.find_element_by_xpath('//*[@id="root"]/div/div[1]/div[2]/a').click()
+        sleep(2)
+
+
+
+        
+BitcoinBot('GOATKING2020', pw)
