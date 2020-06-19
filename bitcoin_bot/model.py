49273e7dbaf8092cdf5c127c50de6f703020390a
Gautham <gauthamkumar.gk.gk@gmail.com>
2020-06-20 00:54:34 +0400

    create dataframe

--- bitcoin_bot/model.py
+++ bitcoin_bot/model.py
@@ -7,3 +7,9 @@ from keras.layers import Dense
 from keras.layer import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+
+# fix random seed for reproducibility
+np.random.seed(7)
+
+dataframe = pd.read_csv('data.txt', header=None)
+print(dataframe)
